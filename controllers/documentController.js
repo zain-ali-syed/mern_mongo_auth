526665adad3cdc6f188c813d68fd772d5ef52604
zain-ali-syed <chelnov63@gmail.com>
2024-12-31 18:24:52 +0000

    added error handling

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,57 +1,72 @@
 // controllers/documentController.js
-const validateObjectId = require('../utils/validateObjectId');
+//controllers are the functions which use our definedßß Models to communicate to the database
+
+const validateObjectId = require("../utils/validateObjectId");
 
 const getAllDocuments = async (req, res, model) => {
-    const documents = await model.find();
-    res.status(200).json(documents);
-}
+  const documents = await model.find().sort({ createdAt: -1 }); // Sort by createdAt in descending order
+  res.status(200).json(documents);
+};
 
 const getDocumentById = async (req, res, model) => {
-    const id = req.params.id;
-    
-    if (!validateObjectId(id)) { 
-      return res.status(404).json({ error: 'No such id' });
-    }
-    
-    const document = await model.findById(id);
-    if (!document) return res.status(404).json({ error: 'No such document' });
+  const id = req.params.id;
 
-    res.status(200).json(document);
-}
+  if (!validateObjectId(id)) {
+    return res.status(404).json({ error: "No such id" });
+  }
+
+  const document = await model.findById(id);
+  if (!document) return res.status(404).json({ error: "No such document" });
+
+  res.status(200).json(document);
+};
 
 const deleteDocumentById = async (req, res, model) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    if (!validateObjectId(id)) { 
-      return res.status(404).json({ error: 'No such id' });
-    }
+  if (!validateObjectId(id)) {
+    return res.status(404).json({ error: "No such id" });
+  }
 
-    const document = await model.findByIdAndDelete(id);
-    if (!document) return res.status(404).json({ error: 'No such document' });
+  const document = await model.findByIdAndDelete(id);
+  if (!document) return res.status(404).json({ error: "No such document" });
 
-    res.status(200).json(document);
-}
+  res.status(200).json(document);
+};
 
 const updateDocumentById = async (req, res, model) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    if (!validateObjectId(id)) { 
-      return res.status(404).json({ error: 'No such id' });
-    }
+  if (!validateObjectId(id)) {
+    return res.status(404).json({ error: "No such id" });
+  }
 
-    const document = await model.findByIdAndUpdate(id, { ...req.body }, { new: true });
-    if (!document) return res.status(404).json({ error: 'No such document' });
+  const document = await model.findByIdAndUpdate(id, { ...req.body }, { new: true });
+  if (!document) return res.status(404).json({ error: "No such document" });
 
-    res.status(200).json(document);
-}
+  res.status(200).json(document);
+};
 
 const addDocument = async (req, res, model) => {
-    try {
-        const document = await model.create(req.body);  // Assume the model accepts a body
-        res.status(200).json(document);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-}
+  const { title, load, reps } = req.body;
+  const errorFields = [];
+  if (!title) errorFields.push("Please fill in title");
+  if (!load) errorFields.push("Please fill in load");
+  if (!reps) errorFields.push("Please fill in reps");
 
-module.exports = { getAllDocuments, getDocumentById, deleteDocumentById, updateDocumentById, addDocument };
+  if (errorFields.length > 0) {
+    console.log("send back ", errorFields);
+    return res.status(400).json({ error: "Please fill in all the fields", errorFields });
+  }
+
+  const document = await model.create(req.body); // Assume the model accepts a body
+  res.status(200).json(document);
+};
+
+module.exports = {
+  getAllDocuments,
+  getDocumentById,
+  deleteDocumentById,
+  updateDocumentById,
+  addDocument,
+};
